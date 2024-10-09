20e00cfc68fe235217843b768ded3e29b9a62adf
karolbsantos <143626291+karolbsantos@users.noreply.github.com>
2024-10-09 09:19:36 -0300

    Add files via upload

--- assets/perguntas.js
+++ assets/perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
     'Programação',
-    'O que é JavaScript?',
-    'O JavaScript é uma linguagem de programação'
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
 )
 
 criaCartao(
@@ -14,4 +14,10 @@ criaCartao(
     'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
